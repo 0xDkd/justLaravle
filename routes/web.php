327bec99b204ad146502304ecab55728e1602e52
aimerforreimu <aimerforreimu@gmail.com>
2018-04-19 16:04:14 +0000

    Add password resets & email configuration

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,21 @@
 */
 
 
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help')->name('help');
-Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/', 'StaticPagesController@home');
+Route::get('help', 'StaticPagesController@help')->name('help');
+Route::get('about', 'StaticPagesController@about')->name('about');
 
-Route::get('/signup','UsersController@create')->name('signup');
-Route::get('/signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
-Route::resource('/users','UsersController');
-Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::resource('users', 'UsersController');
+Route::get('users/{user}/edit', 'UsersController@edit')->name('users.edit');
 
-Route::get('/login','SessionController@create')->name('login');
-Route::post('/login','SessionController@store')->name('login');
-Route::delete('/login','SessionController@destroy')->name('logout');
+Route::get('login', 'SessionController@create')->name('login');
+Route::post('login', 'SessionController@store')->name('login');
+Route::delete('login', 'SessionController@destroy')->name('logout');
+
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
